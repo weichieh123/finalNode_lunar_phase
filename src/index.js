b996026e12d893c08bef9cd44588553155943d35
weichieh123 <weichiehhuang@gapp.nthu.edu.tw>
2021-07-16 17:18:01 +0800

    moana part

--- src/index.js
+++ src/index.js
@@ -37,6 +37,71 @@ app.get('/', (req, res)=>{
     res.render('home',{items: d});
 });
 
+/* =====================Moana的=================== */
+
+// 表單傳遞一般欄位資料
+app.post('/login',async (req, res) => {
+    const output = {
+        success: false,
+        error: '帳號或密碼錯誤',
+        body: req.body
+    }
+
+    const sql = "SELECT * FROM `users` WHERE " + `userEmail = '${req.body.account}' AND userPassword = '${req.body.password}'`
+    const [account] = await dbMysql2.query(sql)
+
+    // 有資料，正列有長度
+    let a = ''
+    if(account.length) {
+        req.session.user = account[0]
+        output.success = true
+        output.error = ""
+        output.data = account[0]
+        res.json(output)
+    } else {
+        res.json(output)
+    }
+})
+// 會員註冊
+app.post('/register',async (req, res) => {
+    const output = {
+        success: false,
+        error: "資料不完整"
+    }
+    let sql = "SELECT `userEmail`,`userPassword` FROM `users` WHERE userEmail = " + `'${req.body.account}'`
+    // 前端欄位都有資料
+    if (req.body.account && req.body.password && req.body.confirm_password) {
+        // res.json({
+        //     acc: req.body.account,
+        //     pwd: req.body.password,
+        //     c_pwd: req.body.confirm_password
+        // })
+
+        // 前端密碼與確認密碼比對，如果沒有帳號，就新增帳號
+        if (req.body.password === req.body.confirm_password) {
+            // 前端帳號與資料庫帳號比對
+            const [account] = await dbMysql2.query(sql)
+            const data = account[0]
+
+            if (account.length) {
+                // res.json(date)
+                output.success = false
+                output.error = '不能使用此帳號'
+                res.json(output)
+            }
+            else {
+                // 測試: SELECT `userEmail`,`userPassword` FROM `users` WHERE userEmail = 'm@gmail.com';
+                sql = "INSERT INTO `users`(`userEmail`, `userPassword`) VALUES " +`('${req.body.account}', '${req.body.password}')`
+                dbMysql2.query(sql)
+                output.success = true
+                output.error = '新增帳號'
+                res.json(output)
+            }
+        }
+    }
+    res.json(output)
+})
+/* =====================Moana的=================== */
 /* =====================大家的路由=================== */
 //J
 app.use('/cart/product/order', require(__dirname + '/routes/order'));
